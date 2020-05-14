447ed50fd5cc805f8ac2308b868c3c9fef1ea8de
Natasha-rare <natasha_ea5@mail.ru>
2020-05-14 08:53:12 +0300

    Практика 15 (новая папка)

---
+++ Практика 15/15.2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Формы_практика2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Указанное число миль равно " + Convert.ToString(Math.Round(1.61 * double.Parse(textBox1.Text), 2)) + " км";
+            textBox1.Text = "";
+        }
+    }
+}
