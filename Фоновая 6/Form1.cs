255cd6335b7489eaf7c523926a1e238bd2033667
Natasha-rare <natasha_ea5@mail.ru>
2020-05-27 16:41:15 +0300

    upd

--- Фоновая 6/Form1.cs
+++ Фоновая 6/Form1.cs
@@ -13,7 +13,7 @@ namespace Фоновая_6
     public partial class Form1 : Form
     {
         int x, y;
-        
+
         public Form1()
         {
             InitializeComponent();
@@ -23,21 +23,22 @@ namespace Фоновая_6
 
         private void button1_Click(object sender, EventArgs e)
         {
-            button2.Left += 4;
-            button2.Top += 2;
             if (button2.Right >= this.ClientSize.Width || button2.Bottom >= this.ClientSize.Height)
             {
                 button2.Location = new Point(x, y);
             }
+            button2.Left += Math.Min(4, this.ClientSize.Width - button2.Right);
+            button2.Top += Math.Min(2, this.ClientSize.Height - button2.Top);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            button2.Top += 6;
             if (button2.Bottom >= this.ClientSize.Height)
             {
                 button2.Location = new Point(x, y);
             }
+            button2.Top += Math.Min(6, this.ClientSize.Height - button2.Top);
+            
         }
     }
 }
